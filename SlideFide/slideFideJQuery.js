6b83faf0d25d142ea383acd67149d9eefb745b20
Valentin Drany <navshop@ukr.net>
2017-02-27 11:57:26 +0200

    Add slide effect fide

---
+++ SlideFide/slideFideJQuery.js
@@ -0,0 +1,43 @@
+
+//Устанавливаем всем блокам который будут участвовать в слайдере стиль 'display: none'
+	$('figure').css('display', 'none').eq(0).css('display', '')
+
+//ф-ция слайдера
+	function slideFide( currentFigure )
+	{
+		if ( currentFigure.next().length )
+		{
+			currentFigure.delay(500).fadeOut(5000, function()
+			{
+				$(this).css(
+					{
+						display: 'none',
+						position: 'absolute'	
+					})
+			})
+			.next().delay(500).fadeIn(5000, function()
+			{
+				$(this).css('position', 'static')
+				slideFide($(this))
+			})	
+		}
+		else
+		{
+			currentFigure.delay(500).fadeOut(5000, function()
+			{
+				$(this).css(
+					{
+						display: 'none',
+						position: 'absolute'	
+					})
+			})
+			.parent().find('figure').eq(0).delay(500).fadeIn(5000, function()
+			{
+				$(this).css('position', 'static')
+				slideFide($(this))
+			})	
+		}
+	}
+	
+// Запуск слайде с первого блока, блок можно менять
+	slideFide( $('figure').eq(0) )
